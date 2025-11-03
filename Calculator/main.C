04fa08e4008ca7e259092a58c41db353b065a9b8
Matthieu_772 <matthieu.dietrich77@gmail.com>
2025-11-03 14:17:54 +0100

    :sparkles: amélioration

--- Calculator/main.C
+++ Calculator/main.C
@@ -1,28 +1,46 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void){
 
     float numberA;
     float numberB;
     char op;
+    bool run = true;
+    char isRunning;
+ 
+    while(run){
+       
+        printf("Write your calcul :\n");
+        scanf("%f %c %f", &numberA, &op, &numberB);
 
-    printf("Entrez votre calcule :\n");
-    scanf("%f %c %f", &numberA, &op, &numberB);
-
-    switch(op){
+        switch(op){
         case '+':
-            printf("%f%c%f = %f", numberA, op, numberB, numberA+numberB);
+            printf("%f%c%f = %f\n", numberA, op, numberB, numberA+numberB);
         break;
         case '-':
-            printf("%f%c%f = %f", numberA, op, numberB, numberA-numberB);
+            printf("%f%c%f = %f\n", numberA, op, numberB, numberA-numberB);
         break;
         case '*':
-            printf("%f%c%f = %f", numberA, op, numberB, numberA*numberB);
+            printf("%f%c%f = %f\n", numberA, op, numberB, numberA*numberB);
         break;
         case '/':
-            printf("%f%c%f = %f", numberA, op, numberB, numberA/numberB);
-        break;
-    }
+            if(numberB == 0){
+                printf("You can't use 0 here with a division\n");
+            } else{printf("%f%c%f = %f\n", numberA, op, numberB, numberA/numberB);
+                }
+            break;
+            }
 
+        printf("write Q to quit pr R to resume (maj+Q or R)\n");
+        scanf(" %c", &isRunning);
+        if(isRunning == 'Q' ){
+            printf("END.");
+            run = false;
+        }else{
+            run = true;
+        }
+    }
+    
     return(0);
 }
